1f219b47de6d088ddf7fcd801099e0ff7bb09d23
unknown <awfrok@gmail.com>
2025-08-01 18:15:10 -0700

    0.1.0

--- main.ts
+++ main.ts
@@ -1,134 +1,99 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+//
+// 0.1.0
+// git status: not committed
+//
 
-// Remember to rename these classes and interfaces!
+import { App, Editor, MarkdownView, Plugin } from 'obsidian';
 
-interface MyPluginSettings {
-	mySetting: string;
-}
+export default class EditHelperPlugin extends Plugin {
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
+	// This method is called when your plugin is loaded.
 	async onload() {
-		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
+		// 1. ADD COMMAND: Empty current line
+		// This command will replace the content of the current line with an empty string.
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
+			id: 'empty-current-line',
+			name: 'Empty current line',
+			hotkeys: [{	modifiers: ['Mod', 'Shift'], key: 'Backspace' }],
 			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
+				const cursor = editor.getCursor();
+				const currentLine = cursor.line;
+				
+				// Replace the line's content with an empty string.
+				editor.setLine(currentLine, '');
+				
+				// Move the cursor to the beginning of the now-empty line.
+				editor.setCursor({ line: currentLine, ch: 0 });
 			}
 		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+		// 2. ADD COMMAND: Delete current line
+		// This command will delete the entire line where the cursor is currently placed.
+		this.addCommand({
+			id: 'delete-current-line',
+			name: 'Delete current line',
+			hotkeys: [{	modifiers: ['Alt', 'Shift'], key: 'Backspace' }],
+			editorCallback: (editor: Editor, view: MarkdownView) => {
+				const cursor = editor.getCursor();
+				const currentLine = cursor.line;
+
+				// This logic handles all cases, including deleting the first, middle, or last line.
+				// It works by getting all lines, removing the target line, and rejoining them.
+				const lines = editor.getValue().split('\n');
+				lines.splice(currentLine, 1);
+				const newValue = lines.join('\n');
+				editor.setValue(newValue);
+
+				// Reposition the cursor in a sensible location after deletion.
+				if (currentLine < lines.length) {
+					// If not the last line, move cursor to the start of the next line.
+					editor.setCursor({ line: currentLine, ch: 0 });
+				} else if (lines.length > 0) {
+					// If the last line was deleted, move to the end of the new last line.
+					const newLastLine = lines.length - 1;
+					editor.setCursor({ line: newLastLine, ch: lines[newLastLine].length });
+				} else {
+					// If the file is now empty, move to the start.
+					editor.setCursor({ line: 0, ch: 0 });
 				}
 			}
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		// 3. ADD COMMAND: Select current line
+		// This command will select the entire line where the cursor is, including the newline character.
+		this.addCommand({
+			id: 'select-current-line',
+			name: 'Select current line',
+			hotkeys: [{modifiers: [], key: 'Escape'}],
+			editorCallback: (editor: Editor, view: MarkdownView) => {
+				const cursor = editor.getCursor();
+				const currentLine = cursor.line;
+				const lineCount = editor.lineCount();
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+				// Define the start of the selection (always the beginning of the line).
+				const from = { line: currentLine, ch: 0 };
+
+				// Define the end of the selection.
+				let to;
+				if (currentLine < lineCount - 1) {
+					// If it's not the last line, select up to the start of the next line
+					// to include the newline character.
+					to = { line: currentLine + 1, ch: 0 };
+				} else {
+					// If it is the last line, select to the very end of that line.
+					const lineContent = editor.getLine(currentLine);
+					to = { line: currentLine, ch: lineContent.length };
+				}
+				
+				// Set the editor's selection.
+				editor.setSelection(from, to);
+			}
 		});
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
+	// This method is called when your plugin is unloaded.
 	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
 	}
 }
